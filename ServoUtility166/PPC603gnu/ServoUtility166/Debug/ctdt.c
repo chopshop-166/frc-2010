32798a2f2cefd01e53edaab0800a2d8fcf957e6b
finn beth <finn.beth@gmail.com>
2009-02-06 20:36:36 +0000

    Added Ben's servo utility

---
+++ ServoUtility166/PPC603gnu/ServoUtility166/Debug/ctdt.c
@@ -0,0 +1,194 @@
+/* ctors and dtors arrays -- to be used by runtime system */
+/*   to call static constructors and destructors          */
+/*                                                        */
+/* NOTE: Use a C compiler to compile this file. If you    */
+/*       are using GNU C++, be sure to use compile this   */
+/*       file using a GNU compiler with the               */
+/*       -fdollars-in-identifiers flag.                   */
+
+
+void _GLOBAL__I__ZN13PIDControllerC2Efff();
+
+void _GLOBAL__I__ZN7Counter11InitCounterEv();
+
+void _GLOBAL__I__ZN4Gyro8InitGyroEv();
+
+void _GLOBAL__I__ZN11SimpleRobotC2Ev();
+
+void _GLOBAL__I__ZN13DigitalOutput17InitDigitalOutputEjj();
+
+void _GLOBAL__I__ZN13Accelerometer17InitAccelerometerEv();
+
+void _GLOBAL__I__Z4Waitj();
+
+void _GLOBAL__I__ZN7Encoder11InitEncoderEb();
+
+void _GLOBAL__I__ZN12DigitalInput16InitDigitalInputEjj();
+
+void _GLOBAL__I__ZN6Jaguar10InitJaguarEv();
+
+void _GLOBAL__I__ZN5Relay9InitRelayEj();
+
+void _GLOBAL__I__ZN10Compressor14InitCompressorEjjjj();
+
+void _GLOBAL__I__ZN3PIDC2Effffi();
+
+void _GLOBAL__I__ZN3I2CC2EP13DigitalModuleh();
+
+void _GLOBAL__I__ZN14IterativeRobotC2Ev();
+
+void _GLOBAL__I__ZN13AnalogChannel11InitChannelEjj();
+
+void _GLOBAL__I__ZN13DigitalModule11GetInstanceEj();
+
+void _GLOBAL__I__ZN16HiTechnicCompassC2Ej();
+
+void _GLOBAL__I__ZN8NotifierC2EPFvPvES0_();
+
+void _GLOBAL__I__ZN23InterruptableSensorBaseC2Ev();
+
+void _GLOBAL__I__ZN12AnalogModule11GetInstanceEj();
+
+void _GLOBAL__I__ZN8Resource14m_resourceListE();
+
+void _GLOBAL__I__ZN12SynchronizedC2EP9semaphore();
+
+void _GLOBAL__I__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__I__ZN13AnalogTrigger11InitTriggerEjj();
+
+void _GLOBAL__I__ZN9GearTooth7InitGTSEb();
+
+void _GLOBAL__I__ZN5Servo9InitServoEv();
+
+void _GLOBAL__I__ZN13DigitalSourceD2Ev();
+
+void _GLOBAL__I__ZN8Solenoid20m_fpgaSolenoidModuleE();
+
+void _GLOBAL__I__ZN10SensorBase21m_defaultAnalogModuleE();
+
+extern void (*_ctors[])();
+void (*_ctors[])() =
+    {
+    _GLOBAL__I__ZN13PIDControllerC2Efff,
+    _GLOBAL__I__ZN7Counter11InitCounterEv,
+    _GLOBAL__I__ZN4Gyro8InitGyroEv,
+    _GLOBAL__I__ZN11SimpleRobotC2Ev,
+    _GLOBAL__I__ZN13DigitalOutput17InitDigitalOutputEjj,
+    _GLOBAL__I__ZN13Accelerometer17InitAccelerometerEv,
+    _GLOBAL__I__Z4Waitj,
+    _GLOBAL__I__ZN7Encoder11InitEncoderEb,
+    _GLOBAL__I__ZN12DigitalInput16InitDigitalInputEjj,
+    _GLOBAL__I__ZN6Jaguar10InitJaguarEv,
+    _GLOBAL__I__ZN5Relay9InitRelayEj,
+    _GLOBAL__I__ZN10Compressor14InitCompressorEjjjj,
+    _GLOBAL__I__ZN3PIDC2Effffi,
+    _GLOBAL__I__ZN3I2CC2EP13DigitalModuleh,
+    _GLOBAL__I__ZN14IterativeRobotC2Ev,
+    _GLOBAL__I__ZN13AnalogChannel11InitChannelEjj,
+    _GLOBAL__I__ZN13DigitalModule11GetInstanceEj,
+    _GLOBAL__I__ZN16HiTechnicCompassC2Ej,
+    _GLOBAL__I__ZN8NotifierC2EPFvPvES0_,
+    _GLOBAL__I__ZN23InterruptableSensorBaseC2Ev,
+    _GLOBAL__I__ZN12AnalogModule11GetInstanceEj,
+    _GLOBAL__I__ZN8Resource14m_resourceListE,
+    _GLOBAL__I__ZN12SynchronizedC2EP9semaphore,
+    _GLOBAL__I__ZN9RobotBase10m_instanceE,
+    _GLOBAL__I__ZN13AnalogTrigger11InitTriggerEjj,
+    _GLOBAL__I__ZN9GearTooth7InitGTSEb,
+    _GLOBAL__I__ZN5Servo9InitServoEv,
+    _GLOBAL__I__ZN13DigitalSourceD2Ev,
+    _GLOBAL__I__ZN8Solenoid20m_fpgaSolenoidModuleE,
+    _GLOBAL__I__ZN10SensorBase21m_defaultAnalogModuleE,
+    0
+    };
+
+void _GLOBAL__D__ZN13PIDControllerC2Efff();
+
+void _GLOBAL__D__ZN7Counter11InitCounterEv();
+
+void _GLOBAL__D__ZN4Gyro8InitGyroEv();
+
+void _GLOBAL__D__ZN11SimpleRobotC2Ev();
+
+void _GLOBAL__D__ZN13DigitalOutput17InitDigitalOutputEjj();
+
+void _GLOBAL__D__ZN13Accelerometer17InitAccelerometerEv();
+
+void _GLOBAL__D__Z4Waitj();
+
+void _GLOBAL__D__ZN7Encoder11InitEncoderEb();
+
+void _GLOBAL__D__ZN12DigitalInput16InitDigitalInputEjj();
+
+void _GLOBAL__D__ZN6Jaguar10InitJaguarEv();
+
+void _GLOBAL__D__ZN5Relay9InitRelayEj();
+
+void _GLOBAL__D__ZN10Compressor14InitCompressorEjjjj();
+
+void _GLOBAL__D__ZN3PIDC2Effffi();
+
+void _GLOBAL__D__ZN3I2CC2EP13DigitalModuleh();
+
+void _GLOBAL__D__ZN14IterativeRobotC2Ev();
+
+void _GLOBAL__D__ZN13AnalogChannel11InitChannelEjj();
+
+void _GLOBAL__D__ZN13DigitalModule11GetInstanceEj();
+
+void _GLOBAL__D__ZN16HiTechnicCompassC2Ej();
+
+void _GLOBAL__D__ZN8NotifierC2EPFvPvES0_();
+
+void _GLOBAL__D__ZN23InterruptableSensorBaseC2Ev();
+
+void _GLOBAL__D__ZN8Resource14m_resourceListE();
+
+void _GLOBAL__D__ZN12SynchronizedC2EP9semaphore();
+
+void _GLOBAL__D__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__D__ZN13AnalogTrigger11InitTriggerEjj();
+
+void _GLOBAL__D__ZN9GearTooth7InitGTSEb();
+
+void _GLOBAL__D__ZN5Servo9InitServoEv();
+
+void _GLOBAL__D__ZN13DigitalSourceD2Ev();
+
+void _GLOBAL__D__ZN10SensorBase21m_defaultAnalogModuleE();
+
+extern void (*_dtors[])();
+void (*_dtors[])() =
+    {
+    _GLOBAL__D__ZN13PIDControllerC2Efff,
+    _GLOBAL__D__ZN7Counter11InitCounterEv,
+    _GLOBAL__D__ZN4Gyro8InitGyroEv,
+    _GLOBAL__D__ZN11SimpleRobotC2Ev,
+    _GLOBAL__D__ZN13DigitalOutput17InitDigitalOutputEjj,
+    _GLOBAL__D__ZN13Accelerometer17InitAccelerometerEv,
+    _GLOBAL__D__Z4Waitj,
+    _GLOBAL__D__ZN7Encoder11InitEncoderEb,
+    _GLOBAL__D__ZN12DigitalInput16InitDigitalInputEjj,
+    _GLOBAL__D__ZN6Jaguar10InitJaguarEv,
+    _GLOBAL__D__ZN5Relay9InitRelayEj,
+    _GLOBAL__D__ZN10Compressor14InitCompressorEjjjj,
+    _GLOBAL__D__ZN3PIDC2Effffi,
+    _GLOBAL__D__ZN3I2CC2EP13DigitalModuleh,
+    _GLOBAL__D__ZN14IterativeRobotC2Ev,
+    _GLOBAL__D__ZN13AnalogChannel11InitChannelEjj,
+    _GLOBAL__D__ZN13DigitalModule11GetInstanceEj,
+    _GLOBAL__D__ZN16HiTechnicCompassC2Ej,
+    _GLOBAL__D__ZN8NotifierC2EPFvPvES0_,
+    _GLOBAL__D__ZN23InterruptableSensorBaseC2Ev,
+    _GLOBAL__D__ZN8Resource14m_resourceListE,
+    _GLOBAL__D__ZN12SynchronizedC2EP9semaphore,
+    _GLOBAL__D__ZN9RobotBase10m_instanceE,
+    _GLOBAL__D__ZN13AnalogTrigger11InitTriggerEjj,
+    _GLOBAL__D__ZN9GearTooth7InitGTSEb,
+    _GLOBAL__D__ZN5Servo9InitServoEv,
+    _GLOBAL__D__ZN13DigitalSourceD2Ev,
+    _GLOBAL__D__ZN10SensorBase21m_defaultAnalogModuleE,
+    0
+    };
