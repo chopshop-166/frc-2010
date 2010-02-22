1c5c12599735b2add600f0000e15f0fedd20300a
benrogers123 <devnull@localhost>
2010-02-22 20:09:04 +0000

    complete rewrite of the state machine in Kicker166.cpp also: addded a delay between latching and releasing the kicker and dTESTED ON TEH REAL BOT

--- chopshop10/Kicker166.cpp
+++ chopshop10/Kicker166.cpp
@@ -98,12 +98,14 @@ int Team166Kicker::Main(int a2, int a3, int a4, int a5,
 	Robot166 *lHandle;                              // Local handle
 	KickerLog sl;                                   // Arm log
 	Solenoid latchSolenoid(T166_LATCH_PISTON);                      // Latch solenoid
-	Solenoid kickerSolenoid(T166_KICKER_PISTON);                     // Kicker solenoid
-	enum {WFP, LATCH, LWAIT, LREL, DSREADY, TRIGGER, KWAIT, KREL} sState = WFP;  // Solenoid state
+	Solenoid unkickSolenoid(T166_KICKER_PISTON);                    // Kicker solenoid
+	Solenoid unlatchSolenoid(T166_UNLATCH_PISTON);                  // Unlatch solenoid
+	//enum {WFP, LATCH, LWAIT, LREL, DSREADY, TRIGGER, KWAIT, KREL} sState = WFP;  // Solenoid state
+	enum {WFP, SIGNAL, TRIGGERWAIT, uLPUSH, KICKWAIT, uKPUSH, uKSENSORWAIT, LPUSH, LWAIT, uKVENT} kickState = TRIGGERWAIT; // Kicker state
 	int lwait;                                      // Latch release wait counter
     int kwait;                                      // Kicker release wait counter
+    int latchwait;                                  // Latched? wait counter
 	DigitalInput Latch_Magnet_Sensor (T166_LATCH_MAGNET_SENSOR);
-	
 	
 	// Let the world know we're in
 	DPRINTF(LOG_DEBUG,"In the 166 Kicker task\n");
@@ -124,9 +126,132 @@ int Team166Kicker::Main(int a2, int a3, int a4, int a5,
 	while ((lHandle->RobotMode == T166_AUTONOMOUS) || 
 			(lHandle->RobotMode == T166_OPERATOR)) {
 		
-		// Get limit switch value
-		Cocked = Latch_Magnet_Sensor.Get();
+		// Get limit switch value TODO GET RID OF ME
+		//Cocked = Latch_Magnet_Sensor.Get();
 		
+        /*
+         *  NEW state machine to control the pistons.
+         */
+		
+		switch (kickState)
+		{
+		// Wait to make sure pressure is adequate
+		case WFP:
+		{
+        	// Is there enough pressure?
+        	if (proxy->GetPressure() < T166_PNEU_KICK_MIN){
+        		// There isn't enough pressure.   :(   Try again later.
+        		break;
+        	}
+        	
+        	// There is enough pressure!!   :)   We're done here.
+        	// Intentionally fall through to SIGNAL
+        	kickState = SIGNAL;
+		}
+		// Signal the DS that we can kick
+		case SIGNAL:
+		{
+        	// TODO send a real ready signal to DS
+        	DPRINTF(LOG_DEBUG, "We're READY TO KICK !!!!!\n");
+        	
+        	// Done
+        	// Intentionally fall through to TRIGGERWAIT
+        	kickState = TRIGGERWAIT;
+        	DPRINTF(LOG_DEBUG, "TRIGGERWAIT: Wait until the trigger (joy 3 - bttn 4) tells us to kick");
+		}
+		// Wait for the trigger to tell us to kick
+		case TRIGGERWAIT:
+		{
+			// Is the trigger asking us to kick?
+			if (!proxy->GetButton(3,4,true)){
+				// The trigger isn't asking us to kick. Wait.
+				break;
+			}
+			// The trigger is asking us to kick.	
+			// Done
+        	// Intentionally fall through to next state
+        	kickState = uLPUSH;
+        	
+		}
+		// (PUSH UNLATCH) unlatch the kicker
+		case uLPUSH:
+		{
+			unkickSolenoid.Set(false); // We aren't unkicking
+			latchSolenoid.Set(false);  // We aren't latching
+			unlatchSolenoid.Set(true); // We ARE unlatching [AND KICKING!!!]
+			
+        	kwait = 0;  // Reset wait counter
+			
+        	// Intentionally fall through to next state
+        	kickState = KICKWAIT;
+		}
+		// Wait for the kicker to kick
+		case KICKWAIT:
+		{
+        	// Have we waited long enough?
+        	if (kwait++ < 100){
+        		
+        		// We haven't waited long enough.   :(    Let's try again later.
+        		break;
+        	}
+        	
+        	// We've waited long enough!!   :D    We're done here.
+			// Intentiionally fall through to the next state
+			kickState = uKPUSH;
+		}
+		// (PUSH UNKICK) pull back the kicker
+		case uKPUSH:
+		{
+			unkickSolenoid.Set(true); // We are unkicking
+        	// Intentionally fall through to next state
+        	kickState = uKSENSORWAIT;
+		}
+		// wait for the sensor tells us the kicker is pulled back
+		case uKSENSORWAIT:
+		{
+        	// Is the magnet sensor ready? Are we ready to override it?
+        	if (Latch_Magnet_Sensor.Get() && (lwait++ < 100)){//T166_LATCH_OVERRIDE)){
+        		// Keep waiting
+        		break;
+        	}
+        	// We have hopefully unkicked
+        	// We've waited long enough!!   :D   We're done here.
+        	// Intentionally fall through to next state
+        	kickState = LPUSH;
+		}
+		// (PUSH LATCH) latch the kicker
+		case LPUSH:
+		{
+			unlatchSolenoid.Set(false); // We aren't unlatching
+			latchSolenoid.Set(true);    // We are latching
+        	// Intentionally fall through to next state
+        	kickState = LWAIT;
+        	latchwait = 0; // Reset the latch wait timer
+		}
+		// Wait for the latch to be down
+		case LWAIT:
+		{
+        	// Have we waited long enough?
+        	if (latchwait++ < 100){
+        		
+        		// We haven't waited long enough.   :(    Let's try again later.
+        		break;
+        	}
+        	
+        	// We've waited long enough!!   :D    We're done here.
+			// Intentiionally fall through to the next state
+        	kickState = uKVENT;
+		}
+		// (VENT UNKICK) release the latched kicker
+		case uKVENT:
+		{
+			unkickSolenoid.Set(false);  // We aren't unkicking
+        	// Intentionally fall through to next state
+        	kickState = WFP;
+		}
+		}
+		
+#if 0
         /*
          *  State machine to control the pistons.
          */
@@ -207,7 +332,7 @@ int Team166Kicker::Main(int a2, int a3, int a4, int a5,
         	// Is the trigger asking us to kick?
         	if (proxy->GetButton(3,4,true)){
         		
-        		//Pressurize the kicker piston
+        		// ------Release the latch
         		kickerSolenoid.Set(true);
         		
             	// Reset the kicker wait counter
@@ -247,20 +372,13 @@ int Team166Kicker::Main(int a2, int a3, int a4, int a5,
         	// Release pressure from the kicker piston
         	kickerSolenoid.Set(false);
         	
-        	
-        	
-        	
-        	
-        	
-        	
-        	
-        	
         	// Done
         	// Intentionally fall through - return to WFP
         	sState = WFP;
         	DPRINTF(LOG_DEBUG, "Returning to the WFP state.\n  WFP: Wait for pressure");
         }
         }
+#endif
 
         // Should we log this value?
 		sl.PutOne(0, 0, 0);
