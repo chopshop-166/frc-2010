44d753a2cc86c660ad5bd8c057baee3e4a885e07
Matt Soucy <demosthenes2k7@gmail.com>
2010-03-25 22:42:27 -0400

    Fixed code so it builds

--- chopshop10/DashboardDataSender.cpp
+++ chopshop10/DashboardDataSender.cpp
@@ -179,10 +179,6 @@ void DashboardDataSender::sendIOPortData(float psi, int tilt) {
 			dash.AddU8(solBuf);
 		}
 		dash.FinalizeCluster();
-	}
-//	dash.AddCluster();
-//	{
-		// PSI values
 		dash.AddFloat(psi);
 		dash.AddI32(tilt);
 	}
