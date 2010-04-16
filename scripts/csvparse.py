1e87f39c153ff6e2a6db7bb5568eca46dd19b9c8
Matt Soucy <demosthenes2k7@gmail.com>
2010-04-16 16:30:36 -0400

    CSV tool now throws its own errors instead of compiler errors

--- scripts/csvparse.py
+++ scripts/csvparse.py
@@ -3,24 +3,28 @@ __all__ = ['parse_robolog', 'parse_latest']
 
 import csv, os
 def parse_robolog(filename):
-    global linenum, ln, name
+    global linenum, ln, keyname
+    print("Checking " + filename)
     reader = csv.DictReader(open(filename))
     names = reader.fieldnames
     linelist = []
     listsize = 0
     linenum = 0
+    keyname = ""
+    ln = []
     for ln in reader:
         linelist.append(ln)
         listsize += 1
     for ln in linelist:
         linenum += 1
-        for name in names:
-            ln[name] = float(ln[name])
+        for keyname in names:
+            ln[keyname] = float(ln[keyname])
     return linelist
 
 def parse_latest():
     # The initial timestamp of all 0s
     # All timestamps are guarenteed to be greater than this!
+    global linenum, ln, name
     max_foldername = "00000000.00.00.00"
     for filename in os.listdir("C:\\FTP"):
         # Find the latest files
@@ -29,8 +33,14 @@ def parse_latest():
     max_foldername = "C:\\FTP\\" + max_foldername
     for filename in os.listdir(max_foldername):
         # Check each file in the latest folder
-        print("Checking " + max_foldername + '\\' + filename)
-        parse_robolog(max_foldername + "\\" + filename)
+        try:
+            parse_robolog(max_foldername + "\\" + filename)
+        except:
+            print("\tError: invalid data on line " + str(linenum))
+            print("\tIn file: " + str(max_foldername + '\\' + filename))
+            print("\tWith key: " + keyname)
+            print("\tLine contents:")
+            print(ln)
 
 if __name__ == "__main__":
     parse_latest()
