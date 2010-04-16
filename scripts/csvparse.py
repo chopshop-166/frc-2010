549590d5fa4c9dabbf7da05856efdfa4d3758afd
Matt Soucy <demosthenes2k7@gmail.com>
2010-04-16 11:24:52 -0400

    Updated the parse_robolog function to use a global variable called linenum, so if a program crashes we'll know why; Python tool now iterates over all files in the latest directory for logs; Proxy166.cpp now has values seperated properly

--- scripts/csvparse.py
+++ scripts/csvparse.py
@@ -1,19 +1,28 @@
 import csv, os
 def parse_robolog(filename):
+    global linenum
     reader = csv.DictReader(open(filename))
     names = reader.fieldnames
     linelist = []
+    listsize = 0
+    linenum = 0
     for ln in reader:
         linelist.append(ln)
+        listsize += 1
     for ln in linelist:
+        linenum += 1
         for name in names:
             ln[name] = float(ln[name])
     for ln in linelist:
         pass
+    return linelist
 
-max_filename = "00000000.00.00.00"
+max_foldername = "00000000.00.00.00"
 for filename in os.listdir("C:\\FTP"):
 #    parse_robolog("W:\\workspace\\test.csv")
-    if filename > max_filename:
-        max_filename = filename
-print max_filename
+    if filename > max_foldername:
+        max_foldername = filename
+max_foldername = "C:\\FTP\\" + max_foldername
+for filename in os.listdir(max_foldername):
+    print("Checking " + max_foldername + '\\' + filename)
+    parse_robolog(max_foldername+"\\"+filename)
