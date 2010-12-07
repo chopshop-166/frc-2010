f8154597c022c15ae169c2e5b58740a01d091246
Ben Beauregard <bot190@gmail.com>
2010-12-07 16:54:02 -0500

    Missed part of logging stuff

--- paradebot10/AxesSensors.cpp
+++ paradebot10/AxesSensors.cpp
@@ -64,9 +64,14 @@ unsigned int AxesSensorsLog::DumpBuffer(char *nptr, FILE *ofile)
 	struct abuf166 *ab = (struct abuf166 *)nptr;
 	
 	// Output the data into the file
-	fprintf(ofile, "%u,%u,%4.5f\n",
+	fprintf(ofile, "%u,%u,%4.5f,%f, %f, %f, %f, %f\n",
 			ab->tp.tv_sec, ab->tp.tv_nsec,
-			((ab->tp.tv_sec - starttime.tv_sec) + ((ab->tp.tv_nsec-starttime.tv_nsec)/1000000000.))
+			((ab->tp.tv_sec - starttime.tv_sec) + ((ab->tp.tv_nsec-starttime.tv_nsec)/1000000000.)),
+			ab->orig_temp,
+			ab->Angle,
+			ab->Voltage,
+			ab->X_Accel,
+			ab->Y_Accel
 			// Add values here
 	);
 	
